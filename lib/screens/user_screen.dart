cf8ed7488e6b5d2a09ab61645e3358181d6dded7
hapo-cuongdm <cuongdm@haposoft.com>
2022-12-26 09:57:27 +0700

    add form create user

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_base/providers/user_provider.dart';
+import 'package:riverpod_base/screens/form_add_user.dart';
 
 class UserScreen extends ConsumerWidget {
   const UserScreen({Key? key}) : super(key: key);
@@ -11,12 +12,42 @@ class UserScreen extends ConsumerWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('User'),
+        actions: [
+          TextButton(
+            onPressed: () => {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => FormAddUser()))
+            },
+            child: const Text(
+              'Add',
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
       ),
       body: Center(
         child: userApiProvider.when(
           data: (data) {
-            print(data);
-            return const Text('data');
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  height: 50,
+                  color: data[index].id % 2 == 0
+                      ? Colors.green[200]
+                      : Colors.amber[200],
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(data[index].username),
+                      Text(data[index].email),
+                    ],
+                  ),
+                );
+              },
+            );
           },
           error: (_, __) => const Text('error'),
           loading: () => const CircularProgressIndicator(),
@@ -25,4 +56,3 @@ class UserScreen extends ConsumerWidget {
     );
   }
 }
-
